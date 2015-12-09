bfae6559baecbaa04e91e2e17b972a0d6c1ef4b4
danieldealday <danieldealday@gmail.com>
2015-12-09 14:37:00 -0800

    changes

--- palette-finder.js
+++ palette-finder.js
@@ -1,1 +1,19 @@
 alert('Im a palette, yo!');
+
+
+var pallette = [];
+
+function grabObjectMax() {
+  var currentMax = {
+    this.colorValue: "",
+    this.max: 0
+  };
+  for (var key in object) {
+    if (key > currentMax.max) {
+      currentMax[max] = key;
+      currentMax[colorValue] = object[key];
+    }
+  }
+  pallette.push(currentMax.colorValue);
+  delete object[currentMax.colorValue];
+}
